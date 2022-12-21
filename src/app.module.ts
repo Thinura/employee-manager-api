bb6a0431f565ead60edde015c0f5dd35c9706e82
thinura <thinuralaksara@gmail.com>
2022-12-22 01:20:39 +0530

    updated docker file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { MulterModule } from '@nestjs/platform-express/multer';
 
 import { EmployeeModule } from './employee/employee.module';
 import { LoggerMiddleware } from './middleware';
-import { TestModule } from './test/test.module';
 
 @Module({
   imports: [
@@ -13,7 +12,6 @@ import { TestModule } from './test/test.module';
     MongooseModule.forRoot(new ConfigService().get('DATABASE_URL')),
     ConfigModule.forRoot({ isGlobal: true }),
     EmployeeModule,
-    TestModule,
   ],
   controllers: [],
   providers: [],
